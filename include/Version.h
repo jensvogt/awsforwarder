7e15a1f97a9cd9e6ba8b71100b4592f2b4ab797e
jensv <jens.vogt@opitz-consulting.com>
2026-01-30 14:36:31 +0100

    fix(PIM-1234): add vcpkg toolchain

--- include/Version.h
+++ include/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.4.9"
+#define APP_VERSION "1.4.18"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 4
-#define APP_VERSION_PATCH 9
+#define APP_VERSION_PATCH 18
